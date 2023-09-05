695a5f14e392bc87d9cb194d93723326cc10035c
sayuz394 <71702420+sayuz394@users.noreply.github.com>
2023-09-05 19:12:07 +0700

    Add files via upload

---
+++ indomath/__init__.py
@@ -0,0 +1,86 @@
+phi = 22/7
+
+def kali(*args):
+    result = 1
+    for i in args:
+        result *= i
+    print(result)
+def tambah(*args):
+    result = 0
+    for i in args:
+        result += i
+    print(result)
+def bagi(*args):
+    if not args:
+        print("No arguments provided. Division result undefined.")
+        return None
+    
+    result = args[0]  
+    for i in args[1:]:  
+        result /= i
+    print(result)
+def akar(a):
+    print(pow(a ,1/2))
+def kurang(*args):
+    result = 0
+    for i in args:
+        result -= i
+    print(result)
+def pangkat(a,b):
+    z = pow(a,b)
+    print(z)
+
+# geometry
+
+
+phi = 22/7
+
+def luas_segitiga(a,t):
+    result = 1/2 * a * t
+    print(result)
+def keliling_segitiga(a,b,c):
+    print(a+b+c)
+def luas_persegi_panjang(p,l):
+    result = p * l 
+    print(result)
+def keliling_persegi_panjang(p,l):
+    print(2 * (p+l))
+def luas_persegi(a):
+    print(a * a)
+def keliling_persegi(s):
+    print(4 * s)
+def luas_jajar_genjang(a,t):
+    print(a * t)
+def keliling_jajar_genjang(a,b,c,d):
+    print(a + b + c + d)
+def luas_lingkaran(r):
+    print(phi * r * r)
+def keliling_lingkaran(r,d):
+    print(2*phi*r)
+def keliling_trapesium(a,b,c,d):
+    print(a + b + c + d)
+def luas_trapesium(a,b,t):
+    print(1/2 * (a+b) * t)
+def luas_layang(x,y):
+    print(1/2*x*y)
+def keliling_layang(a,b,c,d):
+    print(a + b + c + d)
+def luas_belah_ketupat(x,y):
+    print(1/2 * x * y)
+def keliling_belah_ketupat(a,b,c,d):
+    print(a + b + c + d)
+
+# other
+
+import math as mat
+
+def terbesar(*args):
+    print(max(args))
+def terkecil(*args):
+    print(min(args))
+def nilai_cos(derajat):
+    print(mat.cos(derajat))
+def nilai_sin(derajat):
+    print(mat.sin(derajat))
+def nilai_tan(derajat):
+    print(mat.tan(derajat))
