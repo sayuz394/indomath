f59454d423d29e44495d9bd62a14ba6364835afb
sayuz394 <71702420+sayuz394@users.noreply.github.com>
2023-09-06 13:58:32 +0100

    Update __init__.py
    menambahkan matriks

--- indomath/__init__.py
+++ indomath/__init__.py
@@ -1,3 +1,4 @@
+
 phi = 22/7
 
 def kali(*args):
@@ -29,12 +30,11 @@ def kurang(*args):
 def pangkat(a,b):
     z = pow(a,b)
     print(z)
-
-# geometry
-
-
-phi = 22/7
-
+def terbesar(*args):
+    print(max(args))
+def terkecil(*args):
+    print(min(args))
+# geometri
 def luas_segitiga(a,t):
     result = 1/2 * a * t
     print(result)
@@ -70,14 +70,30 @@ def luas_belah_ketupat(x,y):
 def keliling_belah_ketupat(a,b,c,d):
     print(a + b + c + d)
 
-# other
-# triangle
 def pitagoras(a,b):
     d = pow(a,2) + pow(b,2)
     c = pow(d,1/2)
     print(c)
+# matriks
 
-def terbesar(*args):
-    print(max(args))
-def terkecil(*args):
-    print(min(args))
+def matriks2x2_kali(a,b,c,d,e,f,g,h):
+    h1 = a * e + c * f
+    h2 = b * e + d * f
+    h3 =  a * g + a * h
+    h4 = b * g  + d * h
+    print(h1,h3)
+    print(h2,h4)
+def matriks2x2_tambah(a,b,c,d,e,f,g,h):
+    print(a + e,c + g)
+    print(b + d,d + h)
+def matriks2x2_kurang(a,b,c,d,e,f,g,h):
+    print(a - e,c - g)
+    print(b - d,d - h)
+def matriks2x2_skalar(a,b,c,d,nilai):
+    print(nilai * a,nilai * c)
+    print(nilai * b,nilai * c)
+def matriks2x2_adjoint(a,b,c,d):
+    print(d,c * -1)
+    print(b * -1,a)
+def matriks2x2_determinan(a,b,c,d):
+    print(a * d - b * c)
